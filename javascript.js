2ec7c9382402aa4ce96c582a57f469d29b0dc77f
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-02-05 10:58:08 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function showName() {
+ let name = "Per";
+  document.getElementById("name").innerHTML = name;
+}
+
+function showAge() {
+  let age = 20;
+  document.getElementById("age").innerHTML = age;
+}
+
+function showCity() {
+  let city = "Oslo";
+  document.getElementById('city').innerHTML = city;
+}
